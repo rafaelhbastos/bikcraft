2fa877d1bf6233282d05aca3ef408ae7e927687b
rafaelhbastos <rafaelhenribastos12@gmail.com>
2020-12-14 16:10:32 -0300

    adding javascript to some parts

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote", 
+    time: 5000 
+  });
+
+  new SimpleSlide({ 
+   slide: "portifolio", 
+   time: 5000,
+   nav: true 
+  });
+
+}
+
+if (window.SimpleAnime) {
+new SimpleAnime();
+}
